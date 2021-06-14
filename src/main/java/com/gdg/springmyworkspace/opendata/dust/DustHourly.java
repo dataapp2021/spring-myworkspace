1eb18801491be10ebcd6a7e91fa156a7b15d53ab
KO DAE KEUN <dataapp2021@gmail.com>
2021-06-14 15:21:01 +0900

    XML 버전 추가

--- src/main/java/com/gdg/springmyworkspace/opendata/dust/DustHourly.java
+++ src/main/java/com/gdg/springmyworkspace/opendata/dust/DustHourly.java
@@ -62,4 +62,26 @@ public class DustHourly {
 		this.seoul = item.getSeoul();
 		this.ulsan = item.getUlsan();
 	}
+
+	public DustHourly(DustHourlyResponseXml.Item item) {
+		this.dataTime = item.getDataTime();
+		this.itemCode = item.getItemCode();
+		this.busan = item.getBusan();
+		this.chungbuk = item.getChungbuk();
+		this.chungnam = item.getChungnam();
+		this.daegu = item.getDaegu();
+		this.daejeon = item.getDaejeon();
+		this.gangwon = item.getGangwon();
+		this.gwangju = item.getGwangju();
+		this.gyeongbuk = item.getGyeongbuk();
+		this.gyeonggi = item.getGyeonggi();
+		this.gyeongnam = item.getGyeongnam();
+		this.incheon = item.getIncheon();
+		this.jeju = item.getJeju();
+		this.jeonbuk = item.getJeonbuk();
+		this.jeonnam = item.getJeonnam();
+		this.sejong = item.getSejong();
+		this.seoul = item.getSeoul();
+		this.ulsan = item.getUlsan();
+	}
 }
--- src/main/java/com/gdg/springmyworkspace/opendata/dust/DustHourlyServiceXml.java
+++ src/main/java/com/gdg/springmyworkspace/opendata/dust/DustHourlyServiceXml.java
@@ -5,19 +5,18 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
 
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import com.google.gson.Gson;
-
 @Service
-public class DustHourlyService {
-
+public class DustHourlyServiceXml {
 	DustHourlyRepository repo;
 
 	@Autowired
-	public DustHourlyService(DustHourlyRepository repo) {
+	public DustHourlyServiceXml(DustHourlyRepository repo) {
 		this.repo = repo;
 	}
 
@@ -33,8 +32,8 @@ public class DustHourlyService {
 	public void requestDustHourlyData() throws IOException {
 		System.out.println(new Date().toLocaleString() + "--실행--");
 		//
-//		getDustHourlyData("PM10"); // 미세
-//		getDustHourlyData("PM25"); // 초미세
+		getDustHourlyData("PM10"); // 미세
+		getDustHourlyData("PM25"); // 초미세
 	}
 
 	// 데이터를 요청하는 메서드
@@ -49,7 +48,7 @@ public class DustHourlyService {
 		builder.append("&dataGubun=HOUR"); // 시간단위 조회(HOUR)
 		builder.append("&pageNo=1"); // 현재부터 가까운 시간의 페이지만 조회(1페이지)
 		builder.append("&numOfRows=24"); // 현재부터 24시간의 데이터 조회
-		builder.append("&returnType=json"); // 응답 데이터형식으로 JSON으로 받음
+		builder.append("&returnType=xml"); // 응답 데이터형식으로 JSON으로 받음
 		builder.append("&serviceKey=" + serviceKey);
 
 		// 0. 요청 URL 확인
@@ -60,19 +59,23 @@ public class DustHourlyService {
 		HttpURLConnection con = (HttpURLConnection) url.openConnection(); // URL 주소에 접속을 함
 		byte[] result = con.getInputStream().readAllBytes(); // 본문(body)데이터를 바이트 단위로 읽어들임
 
-		// 2. byte[] -> String(JSON)으로 변환
+		// 2. byte[] -> String(XML)으로 변환
 		String data = new String(result);
-		System.out.println(data);
+//		System.out.println(data);
 
-		// 3. String(JSON) -> Object로 변환을 해야함
+		// 3. String(XML) -> String(JSON)으로 변환
+		JSONObject jObject = XML.toJSONObject(data);
+		System.out.println(jObject.toString());
+
+		// 4. String(JSON) -> Object로 변환을 해야함
 		// 구조가 있는 형식(Class로 찍어낸 Object)으로 변환해야 사용할 수 있음
 		// fromJson(JSON문자열, 변환할타입)
-		DustHourlyResponse response = new Gson().fromJson(data, DustHourlyResponse.class);
-		System.out.println(response);
-
-		// 4. 응답객체를 Entity 객체로 변환하여 저장
-		for (DustHourlyResponse.Item item : response.getResponse().getBody().getItems()) {
-			repo.save(new DustHourly(item));
-		}
+//		DustHourlyResponseXml response = new Gson().fromJson(jObject.toString(), DustHourlyResponseXml.class);
+//		System.out.println(response);
+//
+//		// 5. 응답객체를 Entity 객체로 변환하여 저장
+//		for (DustHourlyResponseXml.Item item : response.getResponse().getBody().getItems().getItem()) {
+//			repo.save(new DustHourly(item));
+//		}
 	}
 }
