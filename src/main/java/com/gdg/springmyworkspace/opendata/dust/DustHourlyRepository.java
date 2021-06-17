751d77edc0af3a47f8ed9320442053c575c3fa5b
KO DAE KEUN <dataapp2021@gmail.com>
2021-06-17 12:08:04 +0900

    update repo

--- src/main/java/com/gdg/springmyworkspace/opendata/dust/DustHourlyRepository.java
+++ src/main/java/com/gdg/springmyworkspace/opendata/dust/DustHourlyRepository.java
@@ -3,5 +3,4 @@ package com.gdg.springmyworkspace.opendata.dust;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DustHourlyRepository extends JpaRepository<DustHourly, DustHourlyId> {
-
 }
